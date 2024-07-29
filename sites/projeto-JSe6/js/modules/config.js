37d0d11e08ac16f24961c7f6ea2c20c2bf155ed8
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2024-07-29 20:52:11 -0300

    Novo site

--- sites/projeto-JSe6/js/modules/config.js
+++
@@ -1,2 +0,0 @@
-export const active = 'active';
-export const events = ['touchstart', 'click'];
--- sites/projeto-mentoria/src/js/main.js
+++
@@ -1,8 +0,0 @@
-var number1 = 15,
-    number2 = 15;
-
-function calcular(){
- return (number1 + number2);
-}
-
-console.log(calcular());
